4b17bc69a01dad3eee057ab8e15cbf43eb482681
Nikitos <nikitosyla@mail.ru>
2017-02-23 18:06:01 +0300

    Разделил на Userstory. В одну историю включить её сториборду, api_wrapper, менеджер

---
+++ Ya.disk/YADSettingPresenter.swift
@@ -0,0 +1,42 @@
+//
+//  YADSettingPresenter.swift
+//  Ya.disk
+//
+//  Created by Admin on 23.02.17.
+//  Copyright © 2017 vvz. All rights reserved.
+//
+
+import Foundation
+
+class YADSettingPresenter
+{
+    func assignView(view: YADBaseView)
+    {
+        
+    }
+    
+    func viewLoaded()
+    {
+        
+    }
+    
+    func refreshData()
+    {
+        
+    }
+    
+    func getModel(atIndexPath indexPath: NSIndexPath) -> Any
+    {
+        return 0
+    }
+    
+    func getModelsCount() -> Int
+    {
+        return 0
+    }
+    
+    func loadModels(withOffset offset: Int, and count: Int)
+    {
+        
+    }
+}
